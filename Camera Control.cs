e3496796673e94a7320a512af085dc15b7e9282f
Marcin <marcin05.02@wp.pl>
2020-07-13 03:08:43 +0200

    Camera control✨
    Added Camera control

---
+++ Camera Control.cs
@@ -0,0 +1,23 @@
+using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Runtime.ExceptionServices;
+using System.Numerics;
+using System.Threading.Tasks.Dataflow;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+class CameraControl
+{
+
+    Camera cam;
+    void start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if()
+    }
+}
